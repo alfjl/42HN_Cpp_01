5f7e4d52d258d72c3028f966aa38a5aa78de51c9
alfjl <alflanghans@gmx.de>
2022-02-18 15:24:11 +0100

    Add finalized ex03

---
+++ ex03/HumanB.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "HumanB.hpp"
+
+HumanB::HumanB( std::string name )
+    : _name(name), _weapon(NULL)
+{}
+
+HumanB::~HumanB() {}
+
+void    HumanB::attack( void )
+{
+    if (this->_weapon)
+        std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+    else
+        std::cout << this->_name << " attacks with their bare hands" << std::endl;
+}
+
+void    HumanB::setWeapon( Weapon &weapon )
+{
+    this->_weapon = &weapon;
+}
---
+++ ex03/Weapon.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+class   Weapon
+{
+private:
+    std::string _type;
+
+public:
+    Weapon( std::string type );
+    ~Weapon();
+
+    std::string const    &getType( void ) const;
+    void    setType( std::string const type );
+};
